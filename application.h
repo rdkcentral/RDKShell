9ded029e8c63c4ddd49348e1c0d6bf77d8e04db6
Stephen Foulds <stephen.foulds@consult.red>
2020-09-24 10:46:20 +0000

    Add DAC mime type

--- application.h
+++ application.h
@@ -22,6 +22,7 @@
 namespace RdkShell
 {
     #define RDKSHELL_APPLICATION_MIME_TYPE_NATIVE "application/native"
+    #define RDKSHELL_APPLICATION_MIME_TYPE_DAC_NATIVE "application/dac.native"
     #define RDKSHELL_APPLICATION_MIME_TYPE_HTML "application/html"
     #define RDKSHELL_APPLICATION_MIME_TYPE_LIGHTNING "application/lightning"
 
